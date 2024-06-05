985baf9425cedfa6b780d69da455f46a99f5e066
kepo402 <alawodeolayinka2@gmail.com>
2024-06-05 02:18:35 -0700

    new commit

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,22 +1,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_depth - Measures the depth of a node in a binary tree
- * @tree: Pointer to the node to measure the depth
+ * binary_tree_postorder - Goes through a binary tree using post-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
  *
- * Return: The depth of the node. If tree is NULL, returns 0
+ * Description: Post-order traversal visits the left subtree, then the right subtree, and finally the root node.
  */
-size_t binary_tree_depth(const binary_tree_t *tree)
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
-	size_t depth = 0;
+	if (tree == NULL || func == NULL)
+		return;
 
-	if (tree == NULL)
-		return (0);
-	while (tree->parent)
-	{
-		depth++;
-		tree = tree->parent;
-	}
-	return (depth);
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	func(tree->n);
 }
 
